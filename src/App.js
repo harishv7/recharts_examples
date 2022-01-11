27954eb29d490997509c59c8f90f90b70e68195d
Harish V <harishv207@gmail.com>
2022-01-12 00:10:52 +0800

    Base template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-function App() {
+import React, { useState } from "react";
+import { Box, Container, Heading, Stack, Text, Button } from "@chakra-ui/react";
+import CSVReader from "react-csv-reader";
+import Nav from "./Nav";
+
+export default function CallToActionWithIllustration() {
+  const [data, setData] = useState([]);
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Nav />
+
+      <Container maxW={"5xl"}>
+        <Stack
+          textAlign={"center"}
+          align={"center"}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
+          >
+            My Charts
+          </Heading>
+          <Text color={"gray.500"} fontSize={"3xl"} maxW={"3xl"}>
+            This is a collection of all my charts made with Recharts.
+          </Text>
+          <Box>
+            <CSVReader onFileLoaded={(data) => setData(data)} />
+          </Box>
+        </Stack>
+      </Container>
+    </>
   );
 }
-
-export default App;
---
+++ src/Nav.js
@@ -0,0 +1,31 @@
+import {
+  Box,
+  Flex,
+  Button,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex alignItems={"center"} justifyContent={"space-between"}>
+          <Box />
+          <Flex h={16} alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
